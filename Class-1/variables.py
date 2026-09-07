962beeee0d26300413548e0d0cf7716aa337c8f6
sanzid943 <sanzidmostofa@gmail.com>
2026-09-08 00:11:41 +0600

    add variable program

---
+++ Class-1/variables.py
@@ -0,0 +1,31 @@
+# Problem-1
+
+# name= "rahim"
+# age= 34
+
+# print("name: ", name)
+# print("age: ", age)
+
+
+
+# Problem-2
+
+# name= "rafi"
+# price= 1243
+# quantity= 5
+
+# print("name: ", name)
+# print("price: ", price)
+# print("quantity: ", quantity)
+
+
+
+
+# Problem-3
+
+price= 2345
+quantity=5
+
+total= price * quantity
+
+print("total amount: ", total)

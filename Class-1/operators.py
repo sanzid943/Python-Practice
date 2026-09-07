0edf9f7b12e76c12d5679fa2e787efa6347d6dbb
sanzid943 <sanzidmostofa@gmail.com>
2026-09-08 00:44:30 +0600

    add operator programs

---
+++ Class-1/operators.py
@@ -0,0 +1,38 @@
+# problem-1
+
+print("addition of 10, 3: ", 10+3)
+print("subtraction of 34, 5: ", 34-5)
+print("multiplication of 23, 5: ", 23*5)
+print("division of 34, 4: ", 34/4)
+print("modulo of 34, 5: ", 34%5)
+print("power of 10, 3: ", 10**3)
+print("floor division of 47, 3: ", 47//3)
+
+
+
+# problem-2
+
+num1= input("enter 1st number: ")
+num2= input("enter 2nd number: ")
+
+if num1==num2 :
+    print("equal number")
+
+elif num1>num2:
+    print("1st number is larger")
+
+else :
+    print("2nd number is smaller")
+
+
+
+
+# problem-3
+
+number= int(input("enter a number: "))
+
+if number % 2==0 :
+    print(number, "is even")
+
+else:
+    print(number, "is odd")
